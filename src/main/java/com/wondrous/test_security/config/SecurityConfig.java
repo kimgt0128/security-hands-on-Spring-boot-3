798754df4b0fe44a4c50dd92e16927ecf8ab8006
KKT <kimgt2828@gmail.com>
2024-11-11 22:51:01 +0900

    feat: SecurityFilterChain 커스텀 로그인 설정 1. customLogin 설정 - loginPage : 권한이 필요한 페이지 접근 시 로그인 페이지로(/login)로 자동 접속 - loginProcessingUrl : 로그인 후 처리될 URL
    2. csrf 설정
    - default : 사이트 위변조 방지 설정 해제"

--- src/main/java/com/wondrous/test_security/config/SecurityConfig.java
+++ src/main/java/com/wondrous/test_security/config/SecurityConfig.java
@@ -25,14 +25,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(withDefaults()) //사이트 위변조 방지 설정 해제
-                .httpBasic(withDefaults())
-                .formLogin(withDefaults())
+                .csrf(withDefaults()) //사이트 위변조 방지 설정 해제(개발 환경에서만)
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/main", "/login", "h2-console").permitAll() // 로그인 없이 접근 가능하도록 설정
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
+                )
+                .formLogin((auth) -> auth
+                        .loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
                 );
         return http.build();
     }
